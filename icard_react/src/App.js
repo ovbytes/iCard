505c54c6a5f98c89d90e3d133cf912bfff77c804
Omar Velazquez <ovelazquez.mx@gmail.com>
2024-10-13 01:33:36 -0600

    Changes to create a new branch for develop purposes

--- icard_react/src/App.js
+++ icard_react/src/App.js
@@ -6,6 +6,7 @@ export default function App() {
   return (
     <ClientLayout className="app">
       <h1 className="app_title">El Tejaban de Don Paco</h1>
+      <h2>Creating a new branch for develop purposes</h2>
       <Navigation />
     </ClientLayout>
   );
